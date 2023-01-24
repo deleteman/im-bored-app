7d523f2b20e4a872da9279906e388541f7571cf5
Fernando Doglio <fernando@openreplay.com>
2023-01-24 13:29:07 +0100

    changing code to be compatible wit 1.10

--- src/App.js
+++ src/App.js
@@ -6,57 +6,44 @@ import Suggestion from './components/Suggestion';
 import {useState} from 'react'
 
 import OpenReplay from '@openreplay/tracker';
-import trackerFetch from '@openreplay/tracker-fetch/cjs';
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './components/loginButton';
-import LogoutButton from './components/logoutButton';
+//import OpenReplay from './lib/openreplay-assist.js'
 
+console.log(OpenReplay)
 
 const users = ["fernando.doglio@gmail.com", "adam.sandler@fakeeamil.com", "thisisatest@gmail.com", "tomholland@imspiderman.com"]
 
 //...
 const tracker = new OpenReplay({
-  projectKey: "ALGr6OX4AciTnIyeJZnY"
+  projectKey: "VxaDL82EFmzUsOFeUvSt",
+  __DISABLE_SECURE_MODE: true,
+  ingestPoint: "https://foss.openreplay.com/ingest",
+  network: {
+    capturePayload: true,
+    sanitizer: (data) => {
+      data.url = data.url.replace(/phonenumber=([0-9]+)/, "phonenumber=XXXXXX")
+      return data
+    }
+    }
 });
 let userId  = users[Math.ceil(Math.random() * 3)]
 tracker.setUserID(userId);
-const fetch = tracker.use(trackerFetch({
-  sanitiser: (data) => {
-    data.url = data.url.replace(/phonenumber=([0-9]+)/, "phonenumber=XXXXXX")
-    return data
-  }
-}))
 tracker.start();
 
 function App() {
 
   let [activity, setActivity]  = useState(null)
-  let [notified, setNotified] = useState(false)
-
-  let {isAuthenticated} = useAuth0()
-
-
-  if(isAuthenticated) {
-    if(!notified) {
-      tracker.event('user_logged_in', userId)
-      setNotified(true)
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>I'm bored!</h1>
-          <h3>Find me something to do...</h3> 
-        </header>
-        <SearchForm setResult={setActivity} fetcher={fetch}></SearchForm>
-        {activity && <Suggestion activity={activity} ></Suggestion>}
-        <img src="incorrect-path.jpg" alt="this is broken" />
-        <LogoutButton />
-      </div>
-      )
-  } else {
-    return <LoginButton />
+    
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>I'm bored!</h1>
+        <h3>Find me something to do...</h3> 
+      </header>
+      <SearchForm setResult={setActivity} fetcher={fetch}></SearchForm>
+      {activity && <Suggestion activity={activity} ></Suggestion>}
+    </div>
+    )
   }
   
-}
 
 export default App;
